84a1af47c12676aa0c121f056dd8ff7f59d844f3
Aparesh Bhunia <apareshbhunia@gmail.com>
2025-06-16 23:24:37 +0530

    Add the Map and do postprocessing

---
+++ Assets/MyScripts/LookMode.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class LookMode : MonoBehaviour
+{
+    [SerializeField] PostProcessVolume vol;
+    [SerializeField] PostProcessProfile standard;
+    [SerializeField] PostProcessProfile nightVision;
+    [SerializeField] GameObject nightVisionOverlay;
+
+    private bool nightVisionOn = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vol = GetComponent<PostProcessVolume>();
+        nightVisionOverlay.SetActive(false);
+        vol.profile = standard;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (!nightVisionOn)
+            {
+                nightVisionOverlay.SetActive(true);
+                vol.profile = nightVision;
+                nightVisionOn = true;
+            }
+            else if(nightVisionOn)
+            {
+                this.gameObject.GetComponent<Camera>().fieldOfView = 60;
+                nightVisionOverlay.SetActive(false);
+                vol.profile = standard;
+                nightVisionOn = false;
+            }
+        }
+    }
+}
---
+++ Assets/MyScripts/NightVisionScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NightVisionScript : MonoBehaviour
+{
+
+    [SerializeField] Image zoomBar;
+    [SerializeField] Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //we also can do 
+        //zoomBar = GameObject.Find("ZoomBar").GetComponent<Image>(); 
+    }
+
+    void OnEnable()
+    {
+        zoomBar.fillAmount = 0.6f;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (cam.fieldOfView > 10)
+            {
+                cam.fieldOfView -= 5;
+                zoomBar.fillAmount = cam.fieldOfView / 100;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (cam.fieldOfView < 60)
+            {
+                cam.fieldOfView += 5;
+                zoomBar.fillAmount = cam.fieldOfView / 100;
+            }
+        }
+        
+
+    }
+}
